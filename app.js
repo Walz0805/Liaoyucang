ce5d9ad0f3c5da69241a6563ed7b0bbc3af3a667
王玉川000 <14984515+wang-yuchuan-000@user.noreply.gitee.com>
2026-09-18 12:53:30 +0800

    延展报告布局并恢复数字人

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import {reportPanels,reportInsight} from './report-panels.js?v=20260918-5';
 import {getAssessment} from './assessment-service.js';
 import {assessmentAdapter,safetyCheck,recommendationEngine,overrideRecommendation} from './assessment.js';
 import {getDemoRecommendation} from './recommendation-service.js?v=20260916-1';
-document.head?.insertAdjacentHTML?.('beforeend','<link rel="stylesheet" href="refinement.css?v=20260914-1"><link rel="stylesheet" href="overlay-fixes.css?v=20260918-18">');
+document.head?.insertAdjacentHTML?.('beforeend','<link rel="stylesheet" href="refinement.css?v=20260914-1"><link rel="stylesheet" href="overlay-fixes.css?v=20260918-19">');
 const $=s=>document.querySelector(s),app=$('#app');
 const requestedSample=typeof location!=='undefined'?new URLSearchParams(location.search).get('sample'):null;
 const state={page:'home',companion:null,beforeAssessment:null,afterAssessment:null,goal:'relax',instrument:'chinese',tone:'jue',selectedPlan:null,recommendedPlan:null,recommendationSampleId:/^U00[1-9]$|^U010$/.test(requestedSample||'')?requestedSample:'U002',overrideReason:'',duration:120,immersion:{phase:'C',progress:0}};
